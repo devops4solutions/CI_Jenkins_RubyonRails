4ff94a0e3d5f2cadc5d1f5a58202276e0e32f4a3
Lakshmoji Rao Y <lakshmojirao999@gmail.com>
2018-12-28 13:52:33 +0530

    test commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 require 'simplecov'
 SimpleCov.start
-require_relative '/var/lib/jenkins/workspace/CI_RubyOnRails/app/controllers/string_ops'
+require_relative '/var/lib/jenkins/workspace/demo-ruby-job/app/controllers/string_ops'
